425103d2870b35f9acb7eb2feddf46c57e6ac169
devjamie <dev.j.pewu@gmail.com>
2025-01-29 21:12:10 -0500

    ok

--- src/Demo1/demoTest1.java
+++ src/Demo1/demoTest1.java
@@ -5,6 +5,22 @@ public class demoTest1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Java");
+		
+
+		int a = 10;
+		int b = 20;
+		
+		int result = a+b;
+		
+		System.out.println(result);
+	}
+
+	
+	public void add (int a, int b)  {
+		System.out.println(a+b);
+	
+	
+
 	}
 
 }
